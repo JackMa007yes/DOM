f46ccdc594a591d9d28f182ea6ad1a9a3e7ef24f
JackMa <1921220510@qq.com>
2021-01-08 15:15:39 +0800

    手写DOM库

---
+++ dom库/src/dom.js
@@ -0,0 +1,153 @@
+window.dom = {
+    create(string) {
+        let middle = document.createElement('templete')
+        minddle.innerHTML = string.trim()
+        return minddle.firstChild
+    },
+
+    after(node, node2) {
+        node.parentNode.insertBefore(node2, node.nextSibling)
+    },
+    before(node, node2) {
+        node.parentNode.insertBefore(node2, node)
+    },
+    append(parent, node) {
+        parent.apeendChild(node)
+    },
+
+    wrap(node, parent) {
+        dom.before(node, parent)
+        dom.append(parent, node)
+    },
+
+    remove(node) {
+        node.parentNode.removeChild(node)
+        return node
+    },
+
+    empty(node) {
+        const array = []
+        let a = node.firstChild
+        while (x) {
+            array.push(dom.remove(x))
+            x = node.firstChild
+        }
+    },
+
+    attr(node, name, value) {
+        if (arguments.length === 2) {
+            return node.getAttribute(name)
+        } else if (arguments.length === 3) {
+            node.setAttribute(name, value)
+        }
+    },
+
+    text(node, string) {
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string
+            } else {
+                node.textContent = string
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText
+            } else {
+                return node.textContent
+            }
+        }
+    },
+
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+
+    },
+
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                for (let key in name) {
+                    node.style[key] = name[key]
+                }
+            }
+        }
+    },
+
+    class: {
+        add(node, className) {
+            node.classList.add(classname)
+        },
+        remove(node, className) {
+            node.classList.remove(classname)
+        },
+
+        has(node, className) {
+            return node.classList.contains(className)
+        } // 返回的是一个布尔值，来表示传入的节点是否为该节点的后代节点
+    },
+
+    on(node, eventName, fn) {
+        node.addEventlistenner(eventName, fn)
+    },
+
+    off(node, eventName, fn) {
+        node.removeEventlistener(eventName, fn)
+    },
+
+    find(selector, scope) {
+        return (document || scope).querySelectorAll(selector)
+    },
+
+    parent(node) {
+        return node.parentNode
+    },
+
+    children(node) {
+        return node.children
+    },
+
+    Sibling(node) {
+        return Array.from(node.parentNode.children).filter(n => n !== node)
+    },
+
+    next(node) {
+        let x = node.nextSibling
+        while (x && x.nodetype !== 3) {
+            x = x.nextSibling
+        }
+        return x
+    },
+
+    previous(node) {
+        let x = node.previousSibling
+        while (x && x.nodeType === 3) {
+            x = x.previousSibling
+        }
+        return x
+    },
+
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    },
+
+    index(node) {
+        const list = node.parentNode.children
+        let i
+        for (i = 1; i < list.length; i++) {
+            if (list[i] === node) {
+                break
+            }
+        }
+        return i
+    }
+}
---
+++ dom库/src/main.js
@@ -0,0 +1,20 @@
+const div = dom.create('<tr><td>h1</td></tr>')
+console.log(div)
+    // dom.after(test, div)
+
+// const div3 = dom.create('<div id="parent">div3</div>')
+
+// dom.wrap(test, div3)
+
+// let empty = document.getElementById('empty')
+// let a = dom.empty(empty)
+// console.log(a)
+
+// dom.attr(test, "title", 'hi,I am hahaha')
+
+// const title = dom.attr(test, 'title')
+// console.log('title值为' + title)
+
+// dom.text(test, "你好，这是新的文本内容")
+
+// dom.style(test, { border: '1px solid red', color: blue })
